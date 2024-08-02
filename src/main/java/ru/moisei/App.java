d1ec09d86154393e5665f63bb8e493508122d57d
Andrei M <moisei555@yahoo.com>
2024-08-02 10:38:20 +0500

    lesson4

---
+++ src/main/java/ru/moisei/App.java
@@ -0,0 +1,15 @@
+package ru.moisei;
+
+/**
+ * Урок 4
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}
